4e231f28c5f86ad57890e162375431da74fc5229
Vivek Kumar gupta <vivekkumargupta@VivekGupta-MacBook-Pro.local>
2023-09-09 12:47:39 +0530

    custom dropdown changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DropDownComponent } from './dropdown/dropdown.component';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DropDownComponent
   ],
   imports: [
     BrowserModule,
